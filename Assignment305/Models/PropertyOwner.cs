b9499ac9476d148c03a68e1c1ccc3d692486f48c
tricao1212 <tri.cao.cit20@eiu.edu.vn>
2024-04-23 14:45:07 +0700

    addmodel

---
+++ Assignment305/Models/PropertyOwner.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment305.Models
+{
+    public class PropertyOwner
+    {
+        [Key]
+        public int Id { get; set; }
+
+    }
+}
--- Assignment305/Models/User.cs
+++ Assignment305/Models/User.cs
@@ -1,9 +1,16 @@
-﻿namespace Assignment305.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment305.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Email {  get; set; }
-        public string passWord { get; set; }
+        [Required]
+        public string PassWord { get; set; }
+        [Required]
+        public string Type {  get; set; } 
     }
 }
