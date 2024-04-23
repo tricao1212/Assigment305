62e8827e31400150c445f551bd0fc83f14961296
tricao1212 <tri.cao.cit20@eiu.edu.vn>
2024-04-23 14:22:33 +0700

    inital

---
+++ Assignment305/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment305.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email {  get; set; }
+        public string passWord { get; set; }
+    }
+}
